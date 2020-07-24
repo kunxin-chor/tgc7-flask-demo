56e21e377924a9da99ddca3aefe9e340203e6c7f
kunxin-chor <chorkunxin@yahoo.com>
2020-07-24 09:20:57 +0000

    Added form demo

---
+++ 03-form-processing/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.template.html')
+
+
+# @app.route('/contact-us', methods=["GET", "POST"])
+# def contact_us():
+#     if request.method == "GET":
+#         # display the form
+#         return render_template('contact_us.template.html')
+#     elif request.method == "POST":
+#         return "form recieved"
+
+@app.route('/contact-us', methods=["GET"])
+def contact_us():
+    return render_template('contact_us.template.html')
+
+
+@app.route('/contact-us', methods=["POST"])
+def process_contact_us():
+    fullname = request.form.get('fullname')
+    email = request.form.get('email')
+    return render_template('contact_us_success.template.html', 
+    fullname=fullname, email=email)
+
+
+@app.route('/calculate')
+def show_calculator():
+    return render_template("calculate.template.html")
+
+
+@app.route('/calculate', methods=["POST"])
+def process_calculator():
+    num1 = request.form.get('number1')
+    num2 = request.form.get('number2')
+    total = int(num1) + int(num2)
+    with open('total.txt','a') as fptr:
+        fptr.write(str(total)+'\n')
+    
+    return render_template('results.template.html', number1=num1, number2=num2, total=total)
+
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
