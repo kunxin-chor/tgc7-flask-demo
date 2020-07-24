e3c82963a2a5b8303ac259d89a2cc23bb1ec9495
kunxin-chor <chorkunxin@yahoo.com>
2020-07-24 05:13:52 +0000

    Added parameterized urls

--- 01-basic-demo/app.py
+++ 01-basic-demo/app.py
@@ -7,7 +7,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome home"
+    fname = "Paul"
+    lname = "Chor"
+    return render_template("home.template.html",
+                           first_name=fname, last_name=lname)
+
+
+@app.route('/about')
+def about_us():
+    return render_template('about.template.html')
+
+
+# paramterized routes
+@app.route('/add/<n1>/<n2>')
+def add_two(n1, n2):
+    total = int(n1) + int(n2)
+    return render_template('math.template.html', num1=n1,
+                           num2=n2, result=total)
+
+
+@app.route('/products')
+def show_products():
+    # returning a dictionary will send back a JSON object to the client
+    return {
+        "id": 1,
+        "product-name": "Anvil",
+        "desc": "For dropping on roadrunner's head"
+    }
 
 
 # "magic code" -- boilerplate
--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 In the termainl, we must type in and press ENTER:
 
 `pip3 install flask`
+
+# How to a set up a route that uses a HTML template
+
+1. Create a folder named `templates` that is in the same level as your `app.y`
+
+2. In the `templates` folder create a new file named `about.template.html` (or whatever you want your 
+template file to be called)
+
+3. Fill in the template as you deem fit.
+
+4. Inside the view function, 
+
+```
+@app.route('/about')
+def about_us():
+    return render_template('about.template.html')
+```
